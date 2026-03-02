670825022cc541fdfa7e1b258c2a2d23206cee21
victoreferron-create <victor.e.ferron@gmail.com>
2026-03-02 16:38:16 -0500

    finished Main class

---
+++ src/main/java/engine/Main.java
@@ -0,0 +1,59 @@
+package engine;
+
+import core.GameContainer;
+import core.IKryoGame;
+import gameloader.GameLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        String gamePath = args[0];
+        File gameLocation = null;
+
+        try {
+            gameLocation = new File(gamePath);
+        } catch (NullPointerException npe) {
+            System.err.println("No path has been given!");
+            return;
+        }
+
+        if (!gameLocation.exists()) {
+            System.err.println("Invalid path!");
+            return;
+        }
+
+        URL gameURL;
+
+        try {
+            gameURL = gameLocation.toURI().toURL();
+        } catch (MalformedURLException e) {
+            System.err.println("An internal error has occurred: " + e.getMessage());
+            return;
+        }
+
+        GameContainer gameContainer = null;
+        URLClassLoader gameClassLoader;
+        IKryoGame game;
+        try {
+            gameContainer = GameLoader.loadGame(gameURL);
+            game = gameContainer.getGame();
+            gameClassLoader = gameContainer.getLoader();
+        } catch (IOException | NoSuchMethodException | InvocationTargetException | ClassNotFoundException |
+                 InstantiationException | IllegalAccessException e) {
+            System.err.println("An internal error has occurred: " + e.getMessage());
+        }
+
+        try {
+            gameContainer.close();
+        } catch (Exception e) {
+            System.err.println("An internal error has occurred: " + e.getMessage());
+        }
+    }
+
+}
